1d181d4b5b31f5c8947c9d49cd724af362e67cd4
Matt Basta <me@mattbasta.com>
2013-12-09 11:07:26 -0800

    Change test runner to support casper 1.0.x

--- test/unittests.js
+++ test/unittests.js
@@ -1,23 +1,19 @@
-casper.test.begin('Run the unit tests and make sure tehy all pass.', 10, function suite(test) {
+casper.start("http://localhost:8675/tests", function() {
+    casper.test.assertExists('progress', "Unit tests have initialized");
+});
 
-    casper.start("http://localhost:8675/tests", function() {
-        test.assertExists('progress', "Unit tests have initialized");
-    });
+casper.waitFor(function() {
+    return casper.exists('progress[value="1"]');
+});
 
-    casper.waitFor(function() {
-        return casper.exists('progress[value="1"]');
-    });
+casper.then(function() {
+    casper.test.assertEquals(casper.fetchText('#c_failed'), '0',
+                             'Assert no failures');
+    casper.test.assertEquals(casper.fetchText('#c_started'),
+                             casper.fetchText('#c_passed'),
+                             'Assert all tests ended');
+});
 
-    casper.then(function() {
-        test.assertEquals(casper.fetchText('#c_failed'), '0',
-                          'Assert no failures');
-        test.assertEquals(casper.fetchText('#c_started'),
-                          casper.fetchText('#c_passed'),
-                          'Assert all tests ended');
-    });
-
-    casper.run(function() {
-        test.done();
-    });
-
+casper.run(function() {
+    casper.test.done(2);
 });
